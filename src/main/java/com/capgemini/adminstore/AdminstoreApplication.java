7b789a66201221709de3bb9439e4051afcfc22af
Lalit Nikhare <lalitn246@gmail.com>
2018-08-14 11:28:51 +0530

    Initial project skeleton

---
+++ src/main/java/com/capgemini/adminstore/AdminstoreApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.adminstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminstoreApplication.class, args);
+	}
+}
